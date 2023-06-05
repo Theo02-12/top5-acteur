9bc5cb569dcedf183d2d5a966612dbb5cbd31dd7
Théo Couvelard <mrcouvelard@gmail.com>
2023-06-05 14:48:40 +0200

    presque fini

--- script.js
+++ script.js
@@ -1,16 +1,30 @@
 // Ici vous pouvez écrire du code JavaScript
 const swipe = document.querySelectorAll(".swipe");
+const img = document.querySelectorAll(".img");
 function swipeRight(index){
     for(let i = 0; i < swipe.length; i++){
-        console.log(swipe[1]);
         if(i == index){
-            if(swipe[i].style.width === "200px")
+            if(swipe[i].style.width === "200px"){
                 swipe[i].style.width = "0px";
+            }
             else
                 swipe[i].style.width = "200px";
+
         }else{
             swipe[i].style.width = "0px";
         
         }   
+    }
+    for(let i = 0; i < img.length; i++){
+        if(i == index){
+            if(img[i].style.backgroundImage === "url('/images/color-image.jpg')"){
+                img[i].style.backgroundImage = "url('/images/bw-image.jpg')"
+            }
+            else{
+                img[i].style.backgroundImage = "url('images/color-image.jpg')"
+            }
+        }else{
+            img[i].style.backgroundImage = "url('/images/bw-image.jpg')"
+        }
     }
 }
